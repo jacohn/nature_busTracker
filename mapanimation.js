4cf8342e2abbd2a9281aa42d7e5fe8ace0c5d7ae
jacohn <jacohn@gmail.com>
2023-04-16 13:27:56 -0400

    comment editing

--- mapanimation.js
+++ mapanimation.js
@@ -14,7 +14,7 @@ const busStops = [
   [-71.118625, 42.374863],
 ];
 
-// TODO: add your own access token
+// Add your own access token
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamNvaG4iLCJhIjoiY2xnamthcm5kMTY4YzNmcG11eWVkeDd5ayJ9.GDELMcZ3-MK9WSjNOTr00g";
 // const styles=["mapbox://styles/mapbox/streets-v11","mapbox://styles/mapbox/streets-v11","mapbox://styles/jcohn/clgjnsyxa008c01rum7wailr9","mapbox://styles/jcohn/clgjnuzg8008d01ruri9f96uo","mapbox://styles/jcohn/clgjnw1hd000b01mbp0p1m8yv"]
@@ -22,30 +22,34 @@ let countStyle=0;
 // This is the map instance
 let map = new mapboxgl.Map({
   container: 'map',
+  //this is the basic nature style I made in mapbox 
   style: 'mapbox://styles/jcohn/clgjmvrg3000901oxkgpjy9ya',
   center: [-71.104081, 42.365554],
   zoom: 14,
 });
 
-// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+// adds a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
 let marker = new mapboxgl.Marker().setLngLat(busStops[0]).addTo(map);
 // counter here represents the index of the current bus stop
 let counter = 0;
 function move() {
-  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
-  // Use counter to access bus stops in the array busStops
+  
+  // Uses counter to access bus stops in the array busStops
 
   setTimeout(() => {
     if (counter >= busStops.length) {
       counter = 0;
-      /* if(countStyle>=styles.length)
+    
+      /* code for changing map styles after every iteration.
+      if(countStyle>=styles.length)
       {
         countStyle=0
       } else {countStyle++;}
-      //map.style=styles[countStyle];
+      map[style]=styles[countStyle];
 
     */
     }
+    //moving bus 
     marker.setLngLat(busStops[counter]);
     counter++;
     move();
